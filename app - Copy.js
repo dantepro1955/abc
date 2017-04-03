01dea689ceda055349c5d7da7576fd20cd05fd89
dantepro195 <findyou795@gmail.com>
2017-04-03 15:46:42 +0700

    dfd

---
+++ app - Copy.js
@@ -0,0 +1,106 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+var fs = require("fs");
+
+var PORT = process.env.PORT||80;
+
+server.listen(PORT);
+
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+
+//***
+server.on("connection",function(socket){
+	  console.log("connect moi" );
+	  socket.on("data",function(data){
+
+                 console.log("co data den");
+                 io.sockets.emit('sim900', data);
+                 console.log("PORT LA:");console.log(PORT);
+		});
+	
+		socket.on("close",function(){
+		  console.log("closed");
+		});
+		
+		socket.on("error",function(){
+		  console.log("error");
+		});
+}
+);
+//***
+
+app.get("/", function(req, res){
+	res.sendFile(__dirname + "/index.html");
+        console.log("PORT LA:");	
+console.log(PORT);
+console.log(req);console.log(res);
+console.log("addres_LA:");console.log(__dirname);
+});
+
+
+
+
+io.sockets.on('connection', function (socket) {
+  // ket noi thiet bi hien tai , ngat kn thiet bi cu
+
+  console.log("Co nguoi connect");
+  
+  socket.emit('sim900', "Thanh cong");
+
+  socket.on('disconnect',function(data){
+  console.log('disconnected');
+  }); 
+
+  socket.on('tintuweb',function(data){
+  console.log(data);
+  }); 
+ 
+});
+
+
+
+
+/// *********************
+
+var net =require("net");
+
+var server1 = net.createServer();
+server1.on("connection",function(socket){
+  
+
+	  console.log("NET CONNECTION SOME WHERE");
+	  socket.on("data",function(data){
+
+                 console.log("DATA NET");
+    
+		});
+	
+		socket.on("close",function(){
+		  console.log("closed");
+		});
+		
+		socket.on("error",function(){
+		  console.log("error");
+		});
+}
+);
+
+var PORT2 = 9000;
+
+server1.listen(PORT2,function(){
+   console.log("PORT2 CONNECT SUCCESFULL LOL");
+   console.log(PORT2);
+   var host = server1.address().address
+   var port = server1.address().port
+   
+   console.log("SV2://%s:%s", host, port)
+});
+
+
+//************************
+
--- app.js
+++ app.js
@@ -1,71 +1,5 @@
-var express = require("express");
-var app = express();
-var server = require("http").createServer(app);
-var io = require("socket.io").listen(server);
-var fs = require("fs");
-
-var PORT = process.env.PORT||80;
-
-server.listen(PORT);
-
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-
-//***
-server.on("connection",function(socket){
-	  console.log("connect moi" );
-	  socket.on("data",function(data){
-
-                 console.log("co data den");
-                 io.sockets.emit('sim900', data);
-                 console.log("PORT LA:");console.log(PORT);
-		});
-	
-		socket.on("close",function(){
-		  console.log("closed");
-		});
-		
-		socket.on("error",function(){
-		  console.log("error");
-		});
-}
-);
-//***
-
-app.get("/", function(req, res){
-	res.sendFile(__dirname + "/index.html");
-        console.log("PORT LA:");	
-console.log(PORT);
-console.log(req);console.log(res);
-console.log("addres_LA:");console.log(__dirname);
-});
 
 
-
-
-io.sockets.on('connection', function (socket) {
-  // ket noi thiet bi hien tai , ngat kn thiet bi cu
-
-  console.log("Co nguoi connect");
-  
-  socket.emit('sim900', "Thanh cong");
-
-  socket.on('disconnect',function(data){
-  console.log('disconnected');
-  }); 
-
-  socket.on('tintuweb',function(data){
-  console.log(data);
-  }); 
- 
-});
-
-
-
-
-/// *********************
 
 var net =require("net");
 
@@ -77,6 +11,7 @@ server1.on("connection",function(socket){
 	  socket.on("data",function(data){
 
                  console.log("DATA NET");
+                 console.log(data.length);
     
 		});
 	
@@ -90,15 +25,15 @@ server1.on("connection",function(socket){
 }
 );
 
-var PORT2 = 9000;
+var PORT2 = process.env.PORT || 80;
 
 server1.listen(PORT2,function(){
    console.log("PORT2 CONNECT SUCCESFULL LOL");
    console.log(PORT2);
-   var host = server1.address().address
-   var port = server1.address().port
+   var host = server1.address().address;
+   var port = server1.address().port;
    
-   console.log("SV2://%s:%s", host, port)
+   console.log("SV2://%s:%s", host, port);
 });
 
 
